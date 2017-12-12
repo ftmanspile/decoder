8dd5fdcef49ea9ae4fb3ff3b8f198a5b7e98fb0a
ftmanspile <codfan15@gmail.com>
2017-12-12 00:42:01 -0500

    Changed if statement to while loop

--- decoder.rb
+++ decoder.rb
@@ -1,8 +1,12 @@
 puts "Do you want to encode or decode?"
 encode_decode = gets.chomp
 
-
+while encode_decode != ("encode" || "decode")  # != is not equal to
+	puts "Please enter encode or decode!"
+	encode_decode = gets.chomp
+end
 
 puts "What word/sentence would you like to #{encode_decode}?"
 message = gets.chomp
-message.downcase!
+message.downcase! 
+
